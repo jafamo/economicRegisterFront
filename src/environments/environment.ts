510cf0aac20862353052d78c1fba801fa17310ed
Javier <jafamo@gmail.com>
2025-02-27 16:23:44 +0100

    Deleted environment file

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:9016/api',  // URL de la API en desarrollo
-    apiUrlCategories: 'http://localhost:9016/api/categories',
-    googleMapsApiKey: 'AIzaSyCAmjFu4lqRnBK6nIMGDWsfHUtwhwjsQoo'
-  };
